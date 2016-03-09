9b2fa5452acc56e881a4ea9676ddb0d1a29b53eb
Richard Parnaby-King <r.parnaby-king@suttonsilver.co.uk>
2016-03-09 17:12:15 +0000

    Got it working

--- contentscript.js
+++ contentscript.js
@@ -3,22 +3,28 @@
  * @copyright: Richard Parnaby-King
  * @url: http://richard.parnaby-king.co.uk
  */
- 
+
 //get all link tags that have rel="stylesheet"
 var result = document.evaluate("//link[contains(@rel,\"stylesheet\")]",
                                 document.documentElement,
                                 null,
                                 XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
-//add a red border around each link
+
 for (var i=0; i<result.snapshotLength; i++) {
+    
     var $this = result.snapshotItem(i),
         oldHref = $this.getAttribute('href');
-    if($this.getAttribute('data-oldhref').length) {
+        
+    //Save the exising href in a data attribute so we can append the timestamp to THAT
+    // instead of stripping the old timestamp before we can append the new timestamp
+    if($this.getAttribute('data-oldhref')) {
         oldHref = $this.getAttribute('data-oldhref');
     }
     else {
         $this.setAttribute('data-oldhref', oldHref);
     }
-    $this.setAttribute('href', ''.concat(oldhref, '?', Date.now()));
+    
+    //append timestamp to path
+    $this.setAttribute('href', ''.concat(oldHref, '?', Date.now()));
 }
